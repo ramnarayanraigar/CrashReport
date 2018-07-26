e44c912c88049148594fd5181d587419df91bfb2
Ram Narayan Raigar <atoliyaramnarayan@gmail.com>
2018-07-26 11:52:18 +0530

    Initial commit

---
+++ app/src/main/java/raigar/ramnarayan/crashreport/MainActivity.java
@@ -0,0 +1,17 @@
+package raigar.ramnarayan.crashreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView.setText("ram");
+    }
+}
